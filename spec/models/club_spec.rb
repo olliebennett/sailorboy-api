169bb23c29e3e03b63f0ffd5ccb39a21b4e0b937
Ollie Bennett <olliebennett@gmail.com>
2016-01-11 22:53:59 +0000

    Validate club attributes

--- spec/models/club_spec.rb
+++ spec/models/club_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Club, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Club do
+  it { should validate_presence_of(:name) }
 end
