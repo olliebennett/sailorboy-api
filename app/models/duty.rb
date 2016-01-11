51e7fb1516d440f0a89f7a0bc8dc1d48feed5de8
Ollie Bennett <olliebennett@gmail.com>
2016-01-11 22:51:55 +0000

    Validate duty attributes

--- app/models/duty.rb
+++ app/models/duty.rb
@@ -1,2 +1,5 @@
 class Duty < ActiveRecord::Base
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :name, presence: true
 end
--- spec/models/duty_spec.rb
+++ spec/models/duty_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Duty, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Duty do
+  it { should validate_presence_of(:date) }
+  it { should validate_presence_of(:time) }
+  it { should validate_presence_of(:name) }
 end
